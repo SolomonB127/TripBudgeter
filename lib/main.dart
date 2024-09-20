a05b4bb147cc83f59847bdd805090b01a2705f38
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2024-09-20 14:13:01 -0700

    Sign in page Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:trip_budgeter/pages/sign_in.dart';
 import 'package:trip_budgeter/pages/onboarding.dart';
 import 'package:trip_budgeter/theme/theme.dart';
 
