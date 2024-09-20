f8e689e4a92f3cab4884283e5be18347e75afcf0
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-09-20 14:06:18 +0100

    Added splash page

---
+++ lib/components/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:trip_budgeter/main.dart';
+import 'package:trip_budgeter/pages/home.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Simulate a delay before navigating to the next screen (MainApp)
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Background color of the splash screen
+      body: Center(
+          child: Column(
+        children: [
+          Image.asset("assets/images/Logo.png"),
+        ],
+      )),
+    );
+  }
+}
